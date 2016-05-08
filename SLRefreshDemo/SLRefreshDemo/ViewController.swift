8ad08c927b8e9bb9a93f0319bc2bc58880bb1ed4
xinyoulingxi <992503814@qq.com>
2016-05-08 22:06:47 +0800

    gif

--- SLRefreshDemo/SLRefreshDemo/ViewController.swift
+++ SLRefreshDemo/SLRefreshDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tableView.layoutIfNeeded()
+        tableView.dataSource = self
         refresh = RefreshHeaderView(scrollView: self.tableView, haveNav: true)
         refresh.refreshClosure = {
             self.endRefresh(3)
@@ -35,7 +36,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellId = "cellId"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellId)
+        if cell == nil  {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: cellId)
+        }
+        cell?.textLabel?.text = "数据－－－>\(indexPath.row)"
+        return cell!
+    }
+}
+
 
